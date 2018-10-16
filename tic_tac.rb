672911da16367c0b81b598025ffe017df5a551f2
lea-lp <lea.leipp@gmail.com>
2018-10-16 22:28:17 +0200

    ajout couleurs programme

--- tic_tac.rb
+++ tic_tac.rb
@@ -1,21 +1,22 @@
 require_relative 'game'
 require_relative 'player'
 require_relative 'board'
+require 'colorize'
 
 
 def perform
 
-  puts ">> Welcome to the BEST tic-tac-toe game EVER << \n"
+  puts "\n 🔥  >>> Welcome to the BEST tic-tac-toe game EVER <<< 🔥 \n \n"
   #création player 1
-  puts "Player 1 (you'll be - X -): choose your name"
+  puts "Player 1 (you'll be ☄️ ), choose your name :"
   print "> "
   name_p1 = gets.chomp
-  player1 = Player.new(name_p1, "X")
+  player1 = Player.new(name_p1, "☄️")
   #création player 2
-  puts "Player 2 (you'll be - O -): choose your name"
+  puts "Player 2 (you'll be ❄️ ), choose your name :"
   print "> "
   name_p2 = gets.chomp
-  player2 = Player.new(name_p2, "O")
+  player2 = Player.new(name_p2, "❄️")
   # initialisation du jeu
 	game = Game.new(player1, player2)
 
@@ -24,22 +25,27 @@ def perform
 
 		#choix du joueur (si le tour est pair > player1, & impair > player2)
     player = game.players[(game.nb_turn)%2]
+    if (game.nb_turn)%2 == 1
+      puts "It's your turn #{player.name} #{player.symbol} ! ".cyan
+    else
+      puts "It's your turn #{player.name} #{player.symbol} ! ".light_red
+    end
     #affichage des tableaux BOARD GAME & BOARD POSITION
     game.board.display
 
-    puts "It is your turn #{player.name} ! "
-    puts "Choose where you want to play"
+    # puts "It is your turn #{player.name} (#{player.symbol}) ! "
+    puts "Choose where you want to play".green
     print "> "
 		position = gets.chomp.to_i
     #si choix en dehors du tableau BOARD POSITION, on loop la question
-    while position < 0 || position > 8
-      puts "Wrong choice, choose btw 0 and 8 please :)"
+    while (position < 0 || position > 8)
+      puts "🔢  Wrong choice, choose btw 0 and 8 please :) 🔢".light_blue
       print "> "
       position = gets.chomp.to_i
     end
     #si case déjà occupée, on loop la question
-		while !game.board.tab[position].set_case(player.symbol)
-      puts "Case already taken, please enter one of the remaining numbers on the BOARD POSITION"
+		while (!game.board.tab[position].set_case(player.symbol))
+      puts "🚫  Case already taken, please enter one of the remaining numbers on the BOARD POSITION 🚫".red
       print "> "
       position = gets.chomp.to_i
     end
@@ -56,11 +62,11 @@ def perform
 
   #fin de la partie, affichage du score final
 	if (game.players[0].state)
-		puts "#{game.players[0].name} win !!!"
+		puts "🎉 🎉 🎉  #{game.players[0].name} WINS !!! 🎉 🎉 🎉".upcase.light_red
 	elsif (game.players[1].state)
-		puts "#{game.players[1].name} win !!!"
+		puts "🎉 🎉 🎉  #{game.players[1].name} WINS !!! 🎉 🎉 🎉".upcase.cyan
 	else
-		puts "Tie game ..."
+		puts "💩  TIE GAME 💩"
 	end
 
 end
